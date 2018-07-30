b3ff01188cd05b158b8f5a73bfb19d55108300bb
cutls <cutls>
2018-07-30 21:39:05 +0900

    Delete some files

--- app/node_modules/.bin/pixelmatch.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\pixelmatch\bin\pixelmatch" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\pixelmatch\bin\pixelmatch" %*
-)
