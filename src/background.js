dd1c0aae4e3c042b2cfd37173f17c04bb1d060f3
kPherox <admin@mail.kr-kp.com>
2019-04-05 21:19:58 +0900

    Add context menu

--- src/background.js
+++ src/background.js
@@ -5,11 +5,14 @@ import {
   createProtocol,
   installVueDevtools
 } from 'vue-cli-plugin-electron-builder/lib'
+import contextMenu from 'electron-context-menu'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
+
+contextMenu()
 
 // Standard scheme must be registered before the app is ready
 protocol.registerStandardSchemes(['app'], { secure: true })
