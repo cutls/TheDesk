6269d07d01f757806b0ec6c265501cca21041f87
kPherox <admin@mail.kr-kp.com>
2019-04-09 21:40:23 +0900

    Mive views/loadComponents to components/_globals

---
+++ src/components/_globals.ts
@@ -0,0 +1,21 @@
+'use strict'
+
+import Vue from 'vue';
+import { upperFirst } from 'lodash'
+import { camelCase } from 'lodash'
+
+const requireComponent = require.context(
+  './globals',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach((fileName: string) => {
+  const componentConfig = requireComponent(fileName)
+  const componentName = upperFirst(
+    camelCase(
+      fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
+    )
+  )
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
