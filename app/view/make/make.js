bd8ac2935b32e27946e7eb547a4ca0baec878ba6
Cutls <web-pro@cutls.com>
2019-09-17 01:37:18 +0900

    to Travis

--- app/view/make/make.js
+++ app/view/make/make.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-let ver = "Usamin (18.10.0)"
+let ver = "Usamin (18.10.1)-beta"
 const execSync = require('child_process').execSync;
 let gitHash = execSync("git rev-parse HEAD").toString().trim()
 fs.writeFileSync("../../git", gitHash)
