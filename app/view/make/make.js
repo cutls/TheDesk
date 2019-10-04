fffc94fbdd6114edf38658a1dd841f894fef9763
Cutls <web-pro@cutls.com>
2019-10-05 01:57:34 +0900

    Fix transaltions

--- app/view/make/make.js
+++ app/view/make/make.js
@@ -10,12 +10,13 @@ if (process.argv.indexOf("--automatic") === -1) {
     }
 }
 console.log("Constructing view files " + ver + ": make sure to update package.json")
-const langs = ["ja", "en", "ps", "bg", "cs", "de","es-AR"]
+const langs = ["ja", "en", "ps", "bg", "cs", "de", "es-AR"]
 const langsh = ["日本語", "English", "Crowdin translate system(beta)", "български", "Česky", "Deutsch", "Spanish, Argentina"]
 const simples = ["acct", "index", "setting", "update", "setting"]
 const samples = ["acct.sample.html", "index.sample.html", "setting.sample.html", "update.sample.html", "setting.sample.js"]
 const pages = ["acct.html", "index.html", "setting.html", "update.html", "setting.vue.js"]
 let langstr = ""
+let refKey = []
 for (let n = 0; n < langs.length; n++) {
     let lang = langs[n]
     let targetDir = '../' + lang
@@ -28,16 +29,31 @@ for (let n = 0; n < langs.length; n++) {
 for (let i = 0; i < samples.length; i++) {
     let sample = samples[i]
     let sourceParent = fs.readFileSync(sample, 'utf8')
+    let englishRefer = JSON.parse(fs.readFileSync("language/en/" + simples[i] + ".json", 'utf8'))
     for (let j = 0; j < langs.length; j++) {
         let source = sourceParent
         let lang = langs[j]
         let target = JSON.parse(fs.readFileSync("language/" + lang + "/" + simples[i] + ".json", 'utf8'))
-        Object.keys(target).forEach(function (key) {
-            let str = target[key]
-            str = str.replace(/"/g, '\\"')
-            var regExp = new RegExp("@@" + key + "@@", "g")
-            source = source.replace(regExp, str)
-        })
+        if (lang == "ja") {
+            Object.keys(target).forEach(function (key) {
+                refKey.push(key)
+                let str = target[key]
+                str = str.replace(/"/g, '\\"')
+                var regExp = new RegExp("@@" + key + "@@", "g")
+                source = source.replace(regExp, str)
+            })
+        } else {
+            for (let k = 0; k < refKey.length; k++) {
+                let tarKey = refKey[k]
+                if (target[tarKey]) {
+                    var str = target[tarKey]
+                } else {
+                    var str = englishRefer[tarKey]
+                }
+                var regExp = new RegExp("@@" + tarKey + "@@", "g")
+                source = source.replace(regExp, str)
+            }
+        }
         if (lang == "ps") {
             source = source.replace(/@@comment-start@@/g, "")
             source = source.replace(/@@comment-end@@/g, "")
