ee7ca45b911264a10e0e38ca6b5c97589ce5caef
cutls <web-pro@cutls.com>
2020-01-12 11:38:28 +0900

    npexec #208

--- app/main/np.js
+++ app/main/np.js
@@ -2,11 +2,9 @@ function np(mainWindow) {
 	const electron = require('electron')
 	const join = require('path').join
 	const app = electron.app
-    const fs = require('fs')
-    const fpath = join(app.getPath('userData'), 'npexec')
-    const npExec = fs.readFileSync(fpath, 'utf8')
+	const fs = require('fs')
+	const fpath = join(app.getPath('userData'), 'npexec')
 	const ipc = electron.ipcMain
-
 	const { exec } = require('child_process')
 	ipc.on('itunes', async (e, args) => {
 		console.log('Access')
@@ -18,25 +16,25 @@ function np(mainWindow) {
 				try {
 					const nowplaying = require('itunes-nowplaying-mac')
 					let value = await nowplaying()
-                    if(!value && npExec) {
-                        exec(npExec, (error, stdout, stderr) => {
-                            e.sender.webContents.send('itunes-np', stdout)
-                        })
-                    } else {
-                        const artwork = await nowplaying.getThumbnailBuffer(value.databaseID)
-					    const base64 = artwork.toString('base64')
-					    value.artwork = base64
-                        e.sender.webContents.send('itunes-np', value)
-                    }
+					if (!value && npExec) {
+						const artwork = await nowplaying.getThumbnailBuffer(value.databaseID)
+						const base64 = artwork.toString('base64')
+						value.artwork = base64
+						e.sender.webContents.send('itunes-np', value)
+					}
 				} catch (error) {
 					// エラーを返す
 					console.error(error)
-					e.sender.webContents.send('itunes-np', error)
+					try {
+						const npExec = fs.readFileSync(fpath, 'utf8')
+						exec(npExec, (error, stdout, stderr) => {
+							e.sender.webContents.send('itunes-np', { raw: true, data: stdout })
+						})
+					} catch (e) {}
 				}
 			} else {
 			}
 		}
-    })
-    
+	})
 }
 exports.TheDeskNowPlaying = np
