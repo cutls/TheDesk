3fe5676f44157546000d12a6193b16bf6c3fcba1
cutls <web-pro@cutls.com>
2020-01-23 00:49:36 +0900

    Fix: WIP: album artworks

--- app/main/np.js
+++ app/main/np.js
@@ -12,13 +12,13 @@ function np(mainWindow) {
 					const nowplaying = require('itunes-nowplaying-mac')
 					let value = await nowplaying()
 					const artwork = await nowplaying.getThumbnailBuffer(value.databaseID)
-					const base64 = artwork.toString('base64')
-					value.artwork = base64
+					if(artwork) {
+						const base64 = artwork.toString('base64')
+						value.artwork = base64
+					}
 					e.sender.webContents.send('itunes-np', value)
 				} catch (error) {
-					// エラーを返す
-					console.error(error)
-					e.sender.webContents.send('itunes-np', error)
+				
 				}
 			} else {
 			}
