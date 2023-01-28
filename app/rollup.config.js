7e966217f809acdd41c069da647e8a72f785afac
cutls <web-pro@cutls.com>
2023-01-28 12:26:36 +0900

    build pwa

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -14,6 +14,7 @@ const plugins = process.env.ROLLUP_WATCH === 'true' ? [] : [terser({
 })]
 const config = {
     input: 'js/index.ts',
+    context: 'globalThis',
     output: [
         {
             file: './dist/index.js',
